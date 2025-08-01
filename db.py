82414f36c6800361d58619c072ecb0ae5a6dae3b
Nyi Nyi Htun <64080314+nyinyihtun123@users.noreply.github.com>
2025-08-01 20:39:08 +0900

    db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host='localhost',
-        user='YOUR_DB_USER',
-        password='YOUR_DB_PASSWORD',
-        database='YOUR_DB_NAME'
+        user='underdog',
+        password='12345',
+        database='beautiAI'
     )
